7f5271c993944dbed71ef6179859b6d1ec3e028b
Michael Sorensen <voltx180@gmail.com>
2018-09-16 12:30:14 -0700

    cleaned up some stuff

--- lib/models/movie.dart
+++ lib/models/movie.dart
@@ -1,7 +1,3 @@
-import 'dart:async';
-import 'dart:convert';
-import 'dart:io';
-
 class Result {
   final int page;
   final int total_results;
@@ -19,54 +15,53 @@ class Result {
   }
 }
 
-// class Movie {
-//   final int vote_count;
-//   final int id;
-//   final bool video;
-//   final double vote_average;
-//   final String title;
-//   final double popularity;
-//   final String poster_path;
-//   final String original_language;
-//   final String original_title;
-//   final List<int> genre_ids;
-//   final String backdrop_path;
-//   final bool adult;
-//   final String overview;
-//   final String release_date;
+class Movie {
+  final int vote_count;
+  final int id;
+  final bool video;
+  final double vote_average;
+  final String title;
+  final double popularity;
+  final String poster_path;
+  final String original_language;
+  final String original_title;
+  final List<int> genre_ids;
+  final String backdrop_path;
+  final bool adult;
+  final String overview;
+  final String release_date;
 
-//   Movie(
-//       {this.vote_count,
-//       this.id,
-//       this.video,
-//       this.vote_average,
-//       this.title,
-//       this.popularity,
-//       this.poster_path,
-//       this.original_language,
-//       this.original_title,
-//       this.genre_ids,
-//       this.backdrop_path,
-//       this.adult,
-//       this.overview,
-//       this.release_date});
+  Movie(
+      {this.vote_count,
+      this.id,
+      this.video,
+      this.vote_average,
+      this.title,
+      this.popularity,
+      this.poster_path,
+      this.original_language,
+      this.original_title,
+      this.genre_ids,
+      this.backdrop_path,
+      this.adult,
+      this.overview,
+      this.release_date});
 
-//   factory Movie.fromJson(Map<String, dynamic> json) {
-//     return Movie(
-//         vote_count: json['vote_count'],
-//         id: json['id'],
-//         video: json['video'],
-//         vote_average: json['vote_average'],
-//         title: json['title'],
-//         popularity: json['popularity'],
-//         poster_path: json['poster_path'],
-//         original_language: json['original_language'],
-//         original_title: json['original_title'],
-//         genre_ids: json['genre_ids'],
-//         backdrop_path: json['backdrop_path'],
-//         adult: json['adult'],
-//         overview: json['overview'],
-//         release_date: json['release_date']);
-//   }
-
-// }
+  factory Movie.fromJson(Map<String, dynamic> json) {
+    return Movie(
+        vote_count: json['vote_count'],
+        id: json['id'],
+        video: json['video'],
+        vote_average: json['vote_average'],
+        title: json['title'],
+        popularity: json['popularity'],
+        poster_path: json['poster_path'],
+        original_language: json['original_language'],
+        original_title: json['original_title'],
+        genre_ids: json['genre_ids'],
+        backdrop_path: json['backdrop_path'],
+        adult: json['adult'],
+        overview: json['overview'],
+        release_date: json['release_date']);
+  }
+}
