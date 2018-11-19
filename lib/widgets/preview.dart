a9851a282d8e5112a6ea68c625cc8db7369d4cda
Mikkal24 <voltx180@gmail.com>
2018-11-19 10:41:23 -0800

    working on creating preview results screen

--- lib/widgets/preview.dart
+++ lib/widgets/preview.dart
@@ -9,22 +9,22 @@ class Preview extends StatelessWidget {
       future: fetchPreview(),
       builder: (context, snapshot) {
         if (snapshot.hasData) {
-          if (snapshot.hasData) {
-            final MovieList movieList = snapshot.data;
-            return Container(
-                height: 220.00,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: movieList.movie.length,
-                    itemBuilder: (context, index) {
-                      var movie = movieList.movie[index];
-                      return Container(
-                          child: Image.network(
-                              'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' +
-                                  movie.posterPath));
-                      // child: Text(movieList.movie[index].title));
-                    }));
-          }
+          final MovieList movieList = snapshot.data;
+          return Container(
+              height: 220.00,
+              child: ListView.builder(
+                  scrollDirection: Axis.horizontal,
+                  itemCount: movieList.movie.length,
+                  itemBuilder: (context, index) {
+                    var movie = movieList.movie[index];
+                    return GestureDetector(
+                        onLongPress: () {},
+                        child: Container(
+                            child: Image.network(
+                                'https://image.tmdb.org/t/p/w185_and_h278_bestv2/' +
+                                    movie.posterPath)));
+                    // child: Text(movieList.movie[index].title));
+                  }));
         }
         return LinearProgressIndicator();
       },
--- lib/widgets/search.dart
+++ lib/widgets/search.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:meer/screens/results.dart';
+import 'package:meer/API/the_movie_db.dart';
+import 'package:meer/models/movie.dart';
 
 class Search extends StatefulWidget {
   @override
@@ -9,21 +11,23 @@ class Search extends StatefulWidget {
 }
 
 class SearchState extends State<Search> {
-  void _goToPair(String searchTerm) {
+  Future<MovieList> movieList;
+
+  void _search(String searchTerm) {
     Navigator.push(
         context,
         MaterialPageRoute(
             builder: (BuildContext context) =>
-               ResultsScreen(searchTerm: searchTerm)));
+                ResultsScreen(searchTerm: searchTerm)));
   }
 
   @override
   Widget build(BuildContext context) {
     return Center(
       child: TextField(
-        onSubmitted: _goToPair,
-        decoration: InputDecoration(
-            labelText: "Search Term", icon: Icon(Icons.search)),
+        onSubmitted: _search,
+        decoration:
+            InputDecoration(labelText: "Search Term", icon: Icon(Icons.search)),
       ),
     );
   }
